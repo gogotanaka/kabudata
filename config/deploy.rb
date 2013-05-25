3f846723860e80a0668219b7f43e73139088d651
gogotanaka <qlli.illb@gmail.com>
2013-05-25 13:09:09 +0900

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "ggit@bitbucket.org:tanakakazuki/#{application}.git"
+set :repository, "git@bitbucket.org:tanakakazuki/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
