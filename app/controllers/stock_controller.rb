9ea81de6cf9f48ca85476ea66c7246edb67954e3
gogotanaka <qlli.illb@gmail.com>
2013-06-09 13:04:34 +0900

    fix

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -18,7 +18,30 @@ class StockController < ApplicationController
       return
     end
     html = Nokogiri::HTML(page.read, nil, 'utf-8')
-    @yahoo = html.css("div#yahoo_item_block").to_html
+    @yahoo = html.css("div#yahoo_item_block")[0].css("div.s_res").map{|x|
+      [
+        x.css("span.yahoo_title").inner_text,
+        x.css("p.res_head")[0].inner_text.split(nil)[1],
+        x.css("span")[1].inner_text,
+        x.css("span")[2].inner_text,
+        x.css("div.this_comment p")[0].to_html
+      ]
+    }
+
+    url = URI.encode("http://t-proj.net/twitter/?q=" + @stock.name)
+    begin
+      page = open(url)
+    rescue OpenURI::HTTPError
+      return
+    end
+    html = Nokogiri::HTML(page.read)
+    doc = html.css("div.twitter_status")
+    @twitter = doc[2..doc.length-3].map{|x|
+      [
+        x.css("img")[0].to_html,
+        x.inner_text.split("From")[0]
+      ]
+    }
     
 	end
 
