fa41d31f087bef8bc318d9153c605f7d4d5c9139
gogotanaka <qlli.illb@gmail.com>
2013-08-10 11:43:28 +0900

    fix

--- app/controllers/piece_controller.rb
+++ app/controllers/piece_controller.rb
@@ -2,4 +2,8 @@ class PieceController < ApplicationController
 	def show
 		@piece = Piece.find(params[:id])
 	end
+  def destroy
+    Piece.find(params[:id]).destroy
+    redirect_to :back
+  end
 end
